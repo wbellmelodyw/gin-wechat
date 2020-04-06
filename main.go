016dc2cd478442647bd95de4daf07af29197f5f8
wbellmelodyw <senkai0903>
2020-04-06 23:58:55 +0800

    log

--- main.go
+++ main.go
@@ -4,11 +4,15 @@ import (
 	"github/wbellmelodyw/gin-wechat/config"
 	"github/wbellmelodyw/gin-wechat/logger"
 	"github/wbellmelodyw/gin-wechat/router"
+	"github/wbellmelodyw/gin-wechat/translate"
+	"golang.org/x/text/language"
 	"net/http"
 )
 
 func main() {
-	//text := "hello,world"
+	text := "hello,world"
+	translatedText, _ := translate.Text(text, language.English, language.Chinese)
+	fmt.Println("translated:", translatedText)
 	//fmt.Println(language.Chinese.String())
 	if err := config.Init(); err != nil {
 		logger.Module("system-error").Sugar().Error("load config fail", err)
@@ -17,8 +21,7 @@ func main() {
 	//初始化路由
 	engine := router.Create()
 	//test kk
-	//translatedText, _ := translate.Text(text, language.English, language.Chinese)
-	//fmt.Println("translated:", translatedText)
+
 	//
 	//translatedText2, _ := translate.Text("rookie", language.English, language.Chinese)
 	//fmt.Println("translated2:", translatedText2)
