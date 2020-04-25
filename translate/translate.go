244b85a1d108c6ad00b4239d5299c67c91047b07
sk <senkai0903@vip.qq.com>
2020-04-25 18:11:56 +0800

    log

--- translate/translate.go
+++ translate/translate.go
@@ -3,15 +3,22 @@ package translate
 import (
 	"github.com/go-resty/resty/v2"
 	"github.com/tidwall/gjson"
-	"github/wbellmelodyw/gin-wechat/logger"
+	//"github/wbellmelodyw/gin-wechat/logger"
 	"golang.org/x/text/language"
 	"net/url"
-	"strings"
+	//"strings"
 )
 
 type GoogleTranslator struct {
 	form language.Tag //来源
 	to   language.Tag //要翻译成
+}
+
+type Text struct {
+	mean    string              //词意
+	attr    map[string][]string //词性
+	explain map[string][]string //解释
+	example map[string][]string //造句
 }
 
 func GetGoogle(form, to language.Tag) *GoogleTranslator {
@@ -21,7 +28,7 @@ func GetGoogle(form, to language.Tag) *GoogleTranslator {
 	}
 }
 
-func (g *GoogleTranslator) Text(text string) ([]string, error) {
+func (g *GoogleTranslator) Text(text string) (*Text, error) {
 	token := GetToken(text)
 
 	urll := "https://translate.google.com/translate_a/single"
@@ -46,46 +53,49 @@ func (g *GoogleTranslator) Text(text string) ([]string, error) {
 		SetQueryParamsFromValues(url.Values{
 			"dt": []string{"at", "bd", "ex", "ld", "md", "qca", "rw", "rm", "ss", "t"},
 		}).Get(urll)
-	if err != nil {
-		return []string{}, err
-	}
 	//提取翻译
-	texts := make([]string, 0)
+	texts := new(Text)
+	if err != nil {
+		return texts, err
+	}
+	//texts := make([]string, 0)
 	rspJson := r.String()
 	//词意
 	//result := gjson.Get(rspJson, "0.0")
 	wordMean := "词意:" + gjson.Get(rspJson, "0.0.0").String()
-	texts = append(texts, wordMean)
+	texts.mean = wordMean
 	//词性
 	result := gjson.Get(rspJson, "1")
-	wordAtr := "词性:" //少的用+就行 多才用 strings.builder
+	//wordAtr := "词性:" //少的用+就行 多才用 strings.builder
 	for _, attrs := range result.Array() {
-		wordAtr += attrs.Get("0").String() + ":"
+		//texts.attr["词性"] = append(texts.attr["词性"],)
+		//wordAtr += attrs.Get("0").String() + ":"
 		for _, attr := range attrs.Get("1").Array() {
-			wordAtr += attr.String() + ","
+			texts.attr[attrs.Get("0").String()] = append(texts.attr[attrs.Get("0").String()], attr.String())
+			//wordAtr += attr.String() + ","
 		}
-		wordAtr += ";"
+		//wordAtr += ";"
 	}
-	texts = append(texts, wordAtr)
+	//texts = append(texts, wordAtr)
 	//解释
-	result = gjson.Get(rspJson, "12")
-	wordExplain := "解释:" //少的用+就行 多才用 strings.builder
-	for _, attrs := range result.Array() {
-		wordExplain += attrs.Get("0").String() + ":"
-		for _, attr := range attrs.Get("1").Array() {
-			wordExplain += attr.Get("0").String() + "|"
-		}
-		wordExplain = strings.TrimRight(wordExplain, "|")
-		wordExplain += ";"
-	}
-	logger.Module("test").Sugar().Info("word3", wordExplain)
-	texts = append(texts, wordExplain)
-	//造句
-	wordExample := "造句:"
-	for _, example := range gjson.Get(rspJson, "13.0").Array() {
-		wordExample += example.Get("0").String() + "|"
-	}
-	texts = append(texts, wordExample)
+	//result = gjson.Get(rspJson, "12")
+	//wordExplain := "解释:" //少的用+就行 多才用 strings.builder
+	//for _, attrs := range result.Array() {
+	//	wordExplain += attrs.Get("0").String() + ":"
+	//	for _, attr := range attrs.Get("1").Array() {
+	//		wordExplain += attr.Get("0").String() + "|"
+	//	}
+	//	wordExplain = strings.TrimRight(wordExplain, "|")
+	//	wordExplain += ";"
+	//}
+	//logger.Module("test").Sugar().Info("word3", wordExplain)
+	//texts = append(texts, wordExplain)
+	////造句
+	//wordExample := "造句:"
+	//for _, example := range gjson.Get(rspJson, "13.0").Array() {
+	//	wordExample += example.Get("0").String() + "|"
+	//}
+	//texts = append(texts, wordExample)
 
 	return texts, nil
 }
