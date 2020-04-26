8155ba639bb7f608bdcd5a80d545f0f2bcf61708
wbellmelodyw <senkai0903@vip.qq.com>
2020-04-26 15:21:36 +0800

    audio test

--- translate/translate.go
+++ translate/translate.go
@@ -129,6 +129,7 @@ func (g *GoogleTranslator) Audio(text string) ([]byte, error) {
 		logger.Module("audio").Sugar().Error("read", err)
 		return []byte{}, err
 	}
+	logger.Module("audio").Sugar().Info("read", res)
 	buffer := make([]byte, 40960)
 	_, err = res.RawBody().Read(buffer)
 	if err != nil {
