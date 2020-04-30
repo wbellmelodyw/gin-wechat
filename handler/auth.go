226469f06ca3a1d5b0fdee2337cc02eba44347c9
wbellmelodyw <senkai0903@vip.qq.com>
2020-04-30 14:57:46 +0800

    audio test

---
+++ handler/auth.go
@@ -0,0 +1,39 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/silenceper/wechat"
+	"github.com/silenceper/wechat/message"
+	myconfig "github/wbellmelodyw/gin-wechat/config"
+	"github/wbellmelodyw/gin-wechat/logger"
+)
+
+func WeChatAuth(ctx *gin.Context) {
+	//配置微信参数
+	config := &wechat.Config{
+		AppID:          myconfig.GetString("APP_ID"),
+		AppSecret:      myconfig.GetString("APP_SECRET"),
+		Token:          myconfig.GetString("TOKEN"),
+		EncodingAESKey: myconfig.GetString("ENCODING_AES_KEY"),
+	}
+	wc := wechat.NewWechat(config)
+
+	// 传入request和responseWriter
+	server := wc.GetServer(ctx.Request, ctx.Writer)
+	//设置接收消息的处理方法
+	server.SetMessageHandler(func(msg message.MixMessage) *message.Reply {
+
+		//回复消息：演示回复用户发送的消息
+		text := message.NewText(msg.Content)
+		return &message.Reply{MsgType: message.MsgTypeText, MsgData: text}
+	})
+
+	//处理消息接收以及回复
+	err := server.Serve()
+	if err != nil {
+		logger.Module("wechat").Sugar().Error("serve error", err)
+		return
+	}
+	//发送回复的消息
+	server.Send()
+}
